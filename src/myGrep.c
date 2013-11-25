035b27732458adbc5fb432580ce1bf947e205b61
knavero <knavero@gmail.com>
2013-11-25 11:02:00 -0800

    starting file i/o and crap

--- src/myGrep.c
+++ src/myGrep.c
@@ -11,22 +11,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEBUG 1
+#define PRINT_ARGS 0
+
 int main(int argc, char* argv[]) {
+   // check argument count and error check
    if (argc != 3) {
       printf("Error: Wrong number of arguments.\n"
             "Try: ./a.out <input file> <search word>\n"
-            "For example: ./a.out input.txt the\n");
+            "For example: ./a.out input.txt the\n"
+            "Exiting...");
 
       return 1;
    }
 
+   // if argument count is correct perform grep
    else {
+#if PRINT_ARGS
       printf("Argument count successful! Echo here!\n");
       printf("argc: %d\n", argc);
 
       int i = 0;
       for (i = 0; i < argc; i++) {
          printf("argument %d: %s\n", i, argv[i]);
+      }
+#endif
+      char *file = argv[1];
+      char *search = argv[2];
+
+      FILE *ifstream = fopen(file, "r");
+
+      if (!ifstream) {
+         printf("Error: Could not connect to file for reading. Exiting...");
+         return 1;
       }
 
       return 0;
