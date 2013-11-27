f0224f328077b42be354f9278b899bea2d30c07b
knavero <knavero@gmail.com>
2013-11-26 18:21:24 -0800

    linked list working it seems..time to integrate it into the file i/o and line parsing code

--- src/myGrep.c
+++ src/myGrep.c
@@ -10,43 +10,110 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "myGrep.h"
 
 #define DEBUG 1
-#define PRINT_ARGS 0
+
+LinkedList* createLinkedList(LinkedList *list) {
+   list->head = list->tail = NULL;
+   return list;
+}
+
+Node* createNode(Object *o) {
+   Node *n = (Node*) malloc(sizeof(Node));
+   n->next = n->prev = NULL;
+   n->o = *o;
+
+   return n;
+}
+
+void push_back(LinkedList *list, Node *n) {
+   if (!list->head)
+      list->head = list->tail = n;
+
+   else {
+      list->tail->next = n;
+      n->prev = list->tail;
+      list->tail = list->tail->next;
+   }
+}
+
+void print(LinkedList *list) {
+   Node *n = list->head;
+
+   while(n != NULL) {
+      printf("line: %d, length: %d\n", n->o.line, n->o.length);
+      n = n->next;
+   }
+}
 
 int main(int argc, char* argv[]) {
-   // check argument count and error check
    if (argc != 3) {
       printf("Error: Wrong number of arguments.\n"
             "Try: ./a.out <input file> <search word>\n"
             "For example: ./a.out input.txt the\n"
             "Exiting...");
 
-      return 1;
+      return EXIT_FAILURE;
    }
 
-   // if argument count is correct perform grep
    else {
-#if PRINT_ARGS
-      printf("Argument count successful! Echo here!\n");
-      printf("argc: %d\n", argc);
-
-      int i = 0;
-      for (i = 0; i < argc; i++) {
-         printf("argument %d: %s\n", i, argv[i]);
-      }
-#endif
       char *file = argv[1];
       char *search = argv[2];
+      FILE *ifs = fopen(file, "r");
 
-      FILE *ifstream = fopen(file, "r");
+      printf("%s %s %s\n", argv[0], file, search);
 
-      if (!ifstream) {
-         printf("Error: Could not connect to file for reading. Exiting...");
-         return 1;
+      if (!ifs) {
+         printf("Error: Could not connect to file for reading. Exiting...\n");
+         return EXIT_FAILURE;
       }
 
-      return 0;
+      char line[100];
+      int lineCount = 0;
+
+      while (fgets(line, 100, ifs) != NULL) {
+         if (!feof(ifs))
+            printf("line %d: %s and is %d characters long\n\n",
+                  ++lineCount, line, strlen(line) - 1);
+
+
+         else
+            printf("line %d: %s\n and is %d characters long\n",
+                  ++lineCount, line, strlen(line));
+      }
+
+
+
+
+      // TODO integrate the code below into the actual sentence parsing
+      // and file i/o logic above
+      Sentence s1;
+      s1.length = 3;
+      s1.line = 1;
+
+      Sentence s2;
+      s2.length = 4;
+      s2.line = 5;
+
+      Node *n1 = createNode(&s1);
+      Node *n2 = createNode(&s2);
+
+      LinkedList *ll;
+      createLinkedList(&ll);
+
+      push_back(&ll, n1);
+      push_back(&ll, n2);
+
+      printf("\n\nLinkedList test: \n");
+      print(&ll);
+
+
+
+
+
+      return EXIT_SUCCESS;
 
    }
 
--- src/myGrep.h
+++ src/myGrep.h
@@ -8,8 +8,33 @@
 #ifndef MYGREP_H_
 #define MYGREP_H_
 
+// This struct is user defined. The idea here is to make Node (see below) flexible to
+// any type of data containment
+typedef struct Sentence {
+   int line;
+   int length;
+} Object, Sentence;
 
+// basic Node of a linked list
+typedef struct Node {
+   Object o;
+   struct Node *prev;
+   struct Node *next;
+} Node;
 
+// linked list data structure
+typedef struct LinkedList {
+   Node *head;
+   Node *tail;
+} LinkedList;
 
+LinkedList* createLinkedList(LinkedList* list);
+Node* createNode(Object *o);
+void push_back(LinkedList *list, Node *n);
+
+// TODO clean up memory
+//void clear(LinkedList *list);
+
+void print(LinkedList *list);
 
 #endif /* MYGREP_H_ */
